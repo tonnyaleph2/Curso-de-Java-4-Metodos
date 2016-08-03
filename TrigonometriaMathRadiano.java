bb7074ad541de78431a635a3b217dcac0b8f27cd
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 16:13:07 -0300

    Create TrigonometriaMathRadiano.java

---
+++ TrigonometriaMathRadiano.java
@@ -0,0 +1,9 @@
+public class Trigonometricas {
+    
+    public static void main(String[] args){
+        System.out.println("O seno de 90 é = "+ Math.sin( (Math.PI)/2 ) );
+        System.out.println("O cosseno de 0 é = "+ Math.cos(0) );
+        System.out.println("A tangente de 45 é= "+ Math.tan( (Math.PI)/4 ));
+    }
+
+}
