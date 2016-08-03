edbd19612c48980d8d2a33d1f098aef968d9a519
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 16:36:15 -0300

    Create SobrecargaMetodo.java
    Neste codigo podem ter duas entradas uma inteira ou double, dependendo ele retorna o valor correspondente se por exemplo o numero 2 é inteiro então aparecera inteiro, e o PI(3,1415...) é double, ou seja, os dados podem aparecer conforme a entrada, neste caso já printado sem necessitar de entradas da classe Scanner

---
+++ SobrecargaMetodo.java
@@ -0,0 +1,21 @@
+public class Sobrecarga {
+    
+    public static int quadrado(int num){
+        int quadrado;
+        quadrado = num * num;
+        return quadrado;
+    }
+
+    public static double quadrado(double num){
+        double quadrado;
+        quadrado = num * num;
+        return quadrado;
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Inteiro 2 ao quadrado: " + quadrado(2));
+        System.out.println("Double PI ao quadrado: " + quadrado( Math.PI ));
+    }
+
+}                     
+         
