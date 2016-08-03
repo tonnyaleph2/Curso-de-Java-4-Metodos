9dd77ce1d8e23293cc6aec0915d92ef2432f0df0
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 15:46:59 -0300

    Create PrimeiroMathTest.java

---
+++ PrimeiroMathTest.java
@@ -0,0 +1,7 @@
+public class constantes{
+  public static void main(String[]args){
+    
+    System.out.println("O valor de pi e:" + Math.PI);
+    System.out.println("O valor de pi e:" + Math.E);
+  }
+}
