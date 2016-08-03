6b86d697c9f745c30e5bf1ff89aa9183d38174ff
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 16:11:25 -0300

    Create LogaritmoMathTest.java

---
+++ LogaritmoMathTest.java
@@ -0,0 +1,8 @@
+public class logaritmos {
+    
+    public static void main(String[] args){
+        System.out.println("O logaritmo natural de 10 e = "+ Math.log(10) );
+        System.out.println("O logaritmo natural de 'e' e = "+ Math.log( Math.E ) );
+    }
+
+}
