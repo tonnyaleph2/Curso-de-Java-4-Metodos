e17a2b3f1fed9188fa3baac1eb5045fb4df84d2e
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 15:51:32 -0300

    Create MathExpDefinida.java

---
+++ MathExpDefinida.java
@@ -0,0 +1,8 @@
+public class Mathtest {
+    
+    public static void main(String[] args){
+        System.out.println("'e' elevado ao quadrado = "+ Math.exp(3));
+        System.out.println("2 elevado ao cubo = " + Math.pow(2, 3));
+    }
+
+}
